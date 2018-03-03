6eaf071029376bd8921e5fd5bd28f835ef797b15
ronakupadhyay91 <ronakupadhyay91@gmail.com>
2018-03-03 11:39:58 +0530

    morning changes

---
+++ app/src/main/java/com/branchlocator/branchlocator/Activity/MainDrawerActivity.java
@@ -0,0 +1,24 @@
+package com.branchlocator.branchlocator.Activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;;
+import com.branchlocator.branchlocator.R;
+
+
+public class MainDrawerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+
+
+
+
+    }
+}
+
